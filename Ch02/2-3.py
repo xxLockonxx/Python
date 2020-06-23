a3b84291cdf6c4e8393762884336b5cec6e930f8
xxLockonxx <ptsgrd@naver.com>
2020-06-23 13:19:16 +0900

    2-2 추가

--- Ch02/2-3.py
+++ Ch02/2-3.py
@@ -46,3 +46,70 @@ del numbers[1]
 print('numbers : ', numbers)
 
 # 리스트 관련 함수 교재 p80 ~ 84
+
+# 리스트에 요소 추가(append)
+a = [1,2,3]
+a.append(4)
+print(a)
+a.append([5,6])
+print(a)
+
+# 리스트 정렬(sort)
+a = [1,4,3,2]
+a.sort()
+print(a)
+
+a = ['a', 'c', 'b']
+a.sort()
+print(a)
+
+# 리스트 뒤집기(reverse)
+a = [ 'a', 'c', 'b']
+a.reverse()
+print(a)
+
+# 위치 반환(index)
+a = [1,2,3]
+print(a.index(3))
+print(a.index(1))
+#print(a.index(0))
+
+# 리스트에 요소 삽입(insert)
+
+a = [1,2,3]
+a.insert(0,4)
+print(a)
+a.insert(3,5)
+print(a)
+
+# 리스트 요소 제거
+
+a = [1,2,3,1,2,3]
+a.remove(3)
+print(a)
+a.remove(3)
+print(a)
+
+# 리스트 요소 끄집어내기(pop)
+
+a = [1,2,3]
+a.pop()
+print(a)
+
+a = [1,2,3]
+a.pop(1)
+print(a)
+
+# 리스트에 포함된 요소 x의 개수 세기(count)
+a = [1,2,3,1]
+print(a.count(1))
+
+# 리스트 확장(extend)
+
+a = [1,2,3]
+a.extend([4,5])
+print(a)
+
+b=[6,7]
+a.extend(b)
+print(a)
