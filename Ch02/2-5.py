58872dd473a5051791e265855bc824e2f9b7cc15
xxLockonxx <ptsgrd@naver.com>
2020-06-23 14:05:47 +0900

    2-4추가

--- Ch02/2-5.py
+++ Ch02/2-5.py
@@ -42,3 +42,32 @@ del dic1[3]
 print('dic1 :', dic1)
 
 # 딕셔너리 관련 함수 교재 p93 ~ p96 실습하기
+
+# Key 리스트 만들기(keys)
+a = {'name': 'pey', 'phone':'0119993323', 'birth':'1118'}
+print(a.keys())
+
+# Value 리스트 만들기(values)
+print(a.values())
+
+# Key, Value 쌍 얻기(items)
+print(a.items())
+
+# Key, Value 쌍 모두 지우기(clear)
+print(a.clear())
+
+# Key로 Value 얻기(get)
+a = {'name': 'pey', 'phone':'0119993323', 'birth':'1118'}
+print(a.get('name'))
+print(a.get('phone'))
+print(a.get('nokey'))
+#print(a.['nokey'])
+
+print(a.get('foo','bar'))
+
+# 해당 Key가 딕셔너리 안에 있는지 조사하기(in)
+a = {'name': 'pey', 'phone':'0119993323', 'birth':'1118'}
+print('name' in a)
+print('email' in a)
+
+
