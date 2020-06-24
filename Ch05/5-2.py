645db5d2b91bef2b68aee9a5039f22539a8d4bd7
xxLockonxx <ptsgrd@naver.com>
2020-06-24 14:31:05 +0900

    모듈

--- Ch05/5-2.py
+++ Ch05/5-2.py
@@ -4,3 +4,15 @@
     내용 : 모듈 교재 p207
 """
 
+import Ch05.sub1.Calc as calc
+
+# 모듈(파이썬 패키지에 있는 소스파일) 함수 호출
+rs1 = calc.plus(1, 2)
+rs2 = calc.minus(1, 2)
+rs3 = calc.multi(2, 3)
+rs4 = calc.div(4, 2)
+
+print('rs1 :', rs1)
+print('rs2 :', rs2)
+print('rs3 :', rs3)
+print('rs4 :', rs4)
---
+++ Ch05/sub1/Calc.py
@@ -0,0 +1,13 @@
+# Calc 모듈정의
+def plus(a, b):
+    return a+b
+
+def minus(a, b):
+    return a - b
+
+def multi(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
