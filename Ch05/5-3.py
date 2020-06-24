0974872367641410fdf43f7429d22c3f83d87a0e
xxLockonxx <ptsgrd@naver.com>
2020-06-24 14:33:55 +0900

    모듈

--- Ch05/5-3.py
+++ Ch05/5-3.py
@@ -3,3 +3,15 @@
     이름 : 이성진
     내용 : 패키지 교재 p216
 """
+from Ch05.sub1.Calc import *
+
+r1 = plus(1, 2)
+r2 = minus(1, 2)
+r3 = multi(2, 3)
+r4 = div(4, 2)
+
+print('r1 :', r1)
+print('r2 :', r2)
+print('r3 :', r3)
+print('r4 :', r4)
+
